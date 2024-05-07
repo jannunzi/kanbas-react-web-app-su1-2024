dfa6ba7a396c29669023f801c890bb688b5507a6
Jose Annunziato <jannunzi@gmail.com>
2024-05-06 22:03:07 -0400

    added link to github

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,12 @@ import Labs from "./Labs";
 function App() {
   return (
     <div>
-      <h1>Welcome to Web Dev</h1>
+      <h1>Jose Annunziato</h1>
       <Labs />
+      {/* Kanbas */}
+      <a href="https://github.com/jannunzi/kanbas-react-web-app-su1-2024">
+        GitHub
+      </a>
     </div>
   );
 }
